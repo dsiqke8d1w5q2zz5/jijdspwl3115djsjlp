b6e2adbc9ffae187b6a8ff0f76c70b30383f771f
ohyababy001 <ohyababy001@users.noreply.github.com>
2026-09-17 01:05:51 +0800

    Match transcript total font size to area breakdown

--- 01-crm/sw.js
+++ 01-crm/sw.js
@@ -1,12 +1,12 @@
-var CACHE_NAME = 'crm-v20260917-linkinline1';
+var CACHE_NAME = 'crm-v20260917-totalfont1';
 var URLS_TO_CACHE = [
   './',
   './index.html',
-  './area-editor.js?v=20260917linkinline1',
+  './area-editor.js?v=20260917totalfont1',
   './area-editor.css?v=20260914j',
-  './transcript-parser.js?v=20260917linkinline1',
-  './transcript-import.js?v=20260917linkinline1',
-  './transcript-import.css?v=20260917linkinline1'
+  './transcript-parser.js?v=20260917totalfont1',
+  './transcript-import.js?v=20260917totalfont1',
+  './transcript-import.css?v=20260917totalfont1'
 ];
 
 self.addEventListener('install', function(e) {
